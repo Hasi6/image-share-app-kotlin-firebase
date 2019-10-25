90ea9551c6ad6628bc19da703c88dfe41417f0ba
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-10-25 16:10:21 +0530

    create signup method with firebase

---
+++ app/src/main/java/com/example/instagram_clone/SignInActivity.kt
@@ -0,0 +1,43 @@
+package com.example.instagram_clone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    var mAuth: FirebaseAuth? = null
+    var mAuthListner: FirebaseAuth.AuthStateListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        mAuthListner = FirebaseAuth.AuthStateListener {  }
+    }
+
+//    Sign in clicked
+    fun signInClicked(view: View){
+
+    }
+
+//    Sign up Clicked
+    fun signUpClicked(view:View){
+        val userEmail = email.text.toString()
+        val userPassword = password.text.toString()
+        mAuth?.createUserWithEmailAndPassword(userEmail,userPassword)?.addOnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Toast.makeText(this, "User Created", Toast.LENGTH_LONG).show()
+            }
+        }?.addOnFailureListener{exception ->
+            if(exception !== null){
+                Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
